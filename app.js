10dbed9a22cc9e8afae82ecbdbe8edaad4045db0
Claudiu Lodromanean <claudiulodro@gmail.com>
2017-09-28 16:28:33 -0700

    Lamp post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const BACKGROUND_BASE_COLOR = 0x333333;
 
+var loader = new THREE.JSONLoader();
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
@@ -24,7 +25,7 @@ function addTerrain() {
 	const road_width = 120;
 	const road_length = 360;
 	var road_geometry = new THREE.PlaneGeometry( road_width, road_length, road_width - 1, road_length - 1 );
-	var road_material = new THREE.MeshLambertMaterial( {color: 0xeeeeee, side: THREE.DoubleSide} );
+	var road_material = new THREE.MeshLambertMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
 	var road = new THREE.Mesh( road_geometry, road_material );
 	scene.add( road );
 
@@ -33,10 +34,12 @@ function addTerrain() {
 	const sidewalk_length = road_length;
 	const sidewalk_height = 4;
 	var sidewalk_geometry = new THREE.BoxGeometry( sidewalk_width, sidewalk_length, sidewalk_height );
-	var sidewalk_material = new THREE.MeshLambertMaterial( {color: 0xdddddd} );
+	var sidewalk_material = new THREE.MeshLambertMaterial( {color: 0x00ff00} );
+
 	var sidewalk_left = new THREE.Mesh( sidewalk_geometry, sidewalk_material );
 	sidewalk_left.position.x = - road_width / 2;
 	scene.add( sidewalk_left );
+	
 	var sidewalk_right = new THREE.Mesh( sidewalk_geometry, sidewalk_material );
 	sidewalk_right.position.x = road_width / 2;
 	scene.add( sidewalk_right );
@@ -46,11 +49,13 @@ function addTerrain() {
 	const barrier_length = road_length;
 	const barrier_height = 10;
 	var barrier_geometry = new THREE.BoxGeometry( barrier_width, barrier_length, barrier_height );
-	var barrier_material = new THREE.MeshLambertMaterial( {color: 0xFFFFFF} );
+	var barrier_material = new THREE.MeshLambertMaterial( {color: 0xFF00FF} );
+
 	var barrier_left = new THREE.Mesh( barrier_geometry, barrier_material );
 	barrier_left.position.x = - (road_width / 2) - ( sidewalk_width / 2 );
 	barrier_left.position.y = sidewalk_height;
 	scene.add( barrier_left );
+
 	var barrier_right = new THREE.Mesh( barrier_geometry, barrier_material );
 	barrier_right.position.x = (road_width / 2) + ( sidewalk_width / 2 );
 	barrier_right.position.y = sidewalk_height;
@@ -61,14 +66,44 @@ function addTerrain() {
 	const water_length = road_length;
 	var water_geometry = new THREE.PlaneGeometry( water_width, water_length, water_width - 1, water_length - 1 );
 	var water_material = new THREE.MeshPhongMaterial( {color: 0x0000FF, side: THREE.DoubleSide} );
+
 	var water_left = new THREE.Mesh( water_geometry, water_material );
 	water_left.position.x = - water_width + barrier_left.position.y + 40;
 	scene.add( water_left );
+
 	var water_right = new THREE.Mesh( water_geometry, water_material );
 	water_right.position.x = water_width - barrier_left.position.y - 40;
 	scene.add( water_right );
 
 	//distance ground
+	const south_road_width = 120;
+	const south_road_length = 400
+	var south_road_geometry = new THREE.PlaneGeometry( south_road_width, south_road_length, south_road_width - 1, south_road_length - 1 );
+	var south_road = new THREE.Mesh( south_road_geometry, road_material );
+	south_road.rotation.z = 1.6;
+	south_road.rotation.x = 0.2;
+	south_road.position.y = road_length - south_road_width;
+	south_road.position.z = -15;;	
+	scene.add( south_road );
+
+	const north_road_width = 120;
+	const north_road_length = 400
+	var north_road_geometry = new THREE.PlaneGeometry( north_road_width, north_road_length, north_road_width - 1, north_road_length - 1 );
+	var north_road = new THREE.Mesh( north_road_geometry, road_material );
+	north_road.rotation.z = 1.6;
+	north_road.rotation.x = - 0.2;
+	north_road.position.y = - road_length + south_road_width;
+	north_road.position.z = -15;;	
+	scene.add( north_road );
+
+	//lamp posts
+	loader.load( "model/lamppost.json", function( geometry ) {
+		var model_material = new THREE.MeshLambertMaterial( { color: 0xFFFFFF } );
+		var model_mesh = new THREE.Mesh( geometry, model_material );
+	//	model_mesh.scale.set( 10, 10, 10 );
+		model_mesh.position.set( 0, 0, 0 );
+		scene.add( model_mesh );
+	} );
 
 	//houses
 
@@ -96,7 +131,7 @@ function onWindowResize() {
 function render() {
 	requestAnimationFrame( render );
 	renderer.render( scene, camera );
-	camera.rotation.y += 0.001;
+	camera.rotation.y += 0.01;
 }
 
 setUp();
